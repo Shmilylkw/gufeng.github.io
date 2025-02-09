3b403a3514999658361eadc6830fe1662489e5b0
a''ゞゞ顾风 <88915462+Shmilylkw@users.noreply.github.com>
2025-02-09 13:55:36 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
-echo "n
-p
-1
+# 使用 fdisk 创建新分区
+echo -e "n\np\n1\n\n\nw" | fdisk /dev/sdb
 
-
-w
-" | fdisk /dev/sdb
-
+# 创建物理卷
 pvcreate /dev/sdb1
+
+# 显示卷组信息
 vgs
+
+# 扩展卷组
 vgextend centos /dev/sdb1
+
+# 显示卷组信息
 vgs
+
+# 扩展逻辑卷
 lvextend -l +100%FREE /dev/mapper/centos-root
+
+# 扩展文件系统
 xfs_growfs /dev/mapper/centos-root
-resize2fs  /dev/mapper/centos-root
-vgextend VolGroup00  /dev/sdb1;lvextend -l +100%FREE /dev/mapper/VolGroup00-LogVol00;resize2fs  /dev/mapper/VolGroup00-LogVol00;
 
+# 扩展 ext 文件系统（如果需要）
+resize2fs /dev/mapper/centos-root
 
-
-
+# 扩展 VolGroup00 卷组和逻辑卷并调整文件系统大小
+vgextend VolGroup00 /dev/sdb1
+lvextend -l +100%FREE /dev/mapper/VolGroup00-LogVol00
+resize2fs /dev/mapper/VolGroup00-LogVol00
